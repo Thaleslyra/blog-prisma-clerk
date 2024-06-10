15968b2807e966c33d92949f26b061c8a872fa6a
Thales <thalespipa@hotmail.com>
2024-06-10 15:22:15 -0300

    criado o formulário inicial do post e adicionado a server action na função createPost

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,16 +1,6 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-
 export async function createPost(formData: FormData) {
-  // auth check
-
   const title = formData.get("title") as string;
-  const body = formData.get("body") as string;
-
-  // update database
-
-  // revalidate
-  revalidatePath("/posts");
+  console.log(title);
 }
--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,8 +1,22 @@
-export default async function Page({ params }: { params: { id: string } }) {
+import { createPost } from "@/actions/actions";
+import React from "react";
+
+export default function CreatePostPage() {
   return (
-    <main className="px-7 pt-24 text-center">
-      {/* <h1 className="text-5xl font-semibold mb-7">{post.title}</h1>
-      <p className="max-w-[700px] mx-auto">{post.body}</p> */}
+    <main className="text-center pt-16">
+      <h1 className="text-4xl md:text-5xl font-bold mb-5">Create Post</h1>
+      <form action={createPost} className=" h-10 space-x-2  mt-10">
+        <input
+          type="text"
+          name="title"
+          placeholder="Title for new post"
+          className="border rounded px-3 h-full"
+          required
+        />
+        <button className="h-full bg-blue-500 text-white px-5 rounded">
+          Submit
+        </button>
+      </form>
     </main>
   );
 }
